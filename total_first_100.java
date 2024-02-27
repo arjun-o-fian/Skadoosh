41f64640b7446639fe0e104da7e94a7edcd8677f
Arjun M <143614377+the-arju@users.noreply.github.com>
2024-02-27 10:04:57 +0530

    Add files via upload

---
+++ total_first_100.java
@@ -0,0 +1,11 @@
+
+public class total_first_100 {
+	public static void main(String[] args) {
+		int total=0,n=1;
+		while(n<=100){
+			total+=n;
+			n++;
+		}
+		System.out.println("The total of first 100 numbers is "+total);
+	}
+}
